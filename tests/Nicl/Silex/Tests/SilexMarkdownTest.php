d3fa47860de07bfb8ce87b73c7dac4be7734182b
Nicolas Long <nicolaslong@gmail.com>
2012-10-16 13:43:25 +0100

    initial stab at tests

--- tests/Nicl/Silex/Tests/SilexMarkdownTest.php
+++ tests/Nicl/Silex/Tests/SilexMarkdownTest.php
@@ -3,17 +3,34 @@
 namespace Nicl\Silex\Tests;
 
 use Silex\Application;
-use Nicl\Silex\MarkdownProvider;
+use Silex\Provider\TwigServiceProvider;
+use Nicl\Silex\MarkdownServiceProvider;
 
 class SilexMarkdownTest extends \PHPUnit_Framework_Testcase
 {
     public function setUp()
     {
         $this->app = new Application();
-        $this->app->register(new MarkdownProvider());
+        $this->app->register(new MarkdownServiceProvider());
+        $this->app->register(new TwigServiceProvider());
     }
 
-    public function testMarkdown() {}
+    public function testMarkdown()
+    {
+        $ouput = $this->app['markdown']->transformMarkdown('#Hello World');
+        $this->assertEquals("<h1>Hello World</h1>\n", $output);
+    }
 
-    public function testMarkdownTwigFilter() {}
+    public function testMarkdownTwigFilter()
+    {
+        $twig = $this->app['twig'];
+
+        // tokenise, parse, compile
+        $raw = "{{ '#Hello World'|markdown }}";
+        $stream = $twig->tokenise($raw);
+        $ast = $twig->parse($stream);
+        $output = $twig->compile($ast);
+
+        $this->assertEquals("<h1>Hello World</h1>\n", $output);
+    }
 }
